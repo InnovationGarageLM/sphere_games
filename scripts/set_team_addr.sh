f1f35e5f338f98c5f6039937e30c5e641cb69b66
Innovation Garage <Innovation Garage AT lockheed denver>
2018-11-02 10:17:38 -0600

    Set team addresses

--- scripts/set_team_addr.sh
+++ scripts/set_team_addr.sh
@@ -25,10 +25,10 @@
 #export RED_ADDR="CD:0D:ED:44:B9:17" #F
 
 # DIG
-#export BLUE_ADDR="F2:3C:C5:25:14:0A"
+export BLUE_ADDR="F2:3C:C5:25:14:0A"
 #export BLUE_ADDR="D8:15:37:A1:02:A5"
 #export BLUE_ADDR="E4:A3:F5:2A:3B:7D"
 
-#export RED_ADDR="F0:92:18:F9:7A:E9"
-#export RED_ADDR="D1:AD:9C:AF:16:95"
+export RED_ADDR="F0:92:18:F9:7A:E9"
+#export RED_ADDR="D1:AD:9C:AF:16:95" # Potentially bad heading
 #export RED_ADDR="CA:3E:EC:8C:C4:A6"
